8dda0caf3e56ca92dfe645d353a094cb2592335b
krill3x <67130418+krill3x@users.noreply.github.com>
2020-08-12 21:31:51 -0300

    Create Prelúdio.sh

---
+++ Prelúdio.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#_xXkrill3Xx_
+#1.0
+echo "PRINCIPAIS FERRAMENTAS QUE UM INICIANTE PRECISA :^" 
+echo "confirme a instalação com ( Y ) e não confirme com ( N )"
+echo "leia os comandos e treine"
+echo "execute-> (bash bilin)" && echo "Feito por _xXKrill3Xx_" && sleep 10s;
