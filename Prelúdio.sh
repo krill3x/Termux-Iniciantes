6c348d65d1ae7ed431b11d71d6934dcb6db5cf7f
krill3x <67130418+krill3x@users.noreply.github.com>
2020-08-12 22:38:52 -0300

    Update Prelúdio.sh

--- Prelúdio.sh
+++ Prelúdio.sh
@@ -3,5 +3,14 @@
 #1.0
 echo "PRINCIPAIS FERRAMENTAS QUE UM INICIANTE PRECISA :^" 
 echo "confirme a instalação com ( Y ) e não confirme com ( N )"
+acho "quando for usar termux sempre usar (pkg upgrade && pkg update)"
+pkg upgrade && pkg update
 echo "leia os comandos e treine"
-echo "execute-> (bash bilin)" && echo "Feito por _xXKrill3Xx_" && sleep 10s;
+echo "para instalação de uma ferramenta sempre usar (git clone www.github.com/exemplo)"
+echo "para remover (rm exemplo) caso não saia use (rm -rf exemplo)"
+echo "mover repositório (mv exemplo $HOME/storage/downloads)"
+echo "se estiver perdido entre as pastas use (pwd), se quiser voltar a pasta anterior use (cd ..), volta para $HOME(cd), abrir pastas (cd exemplo)"
+echo "quando abrir uma pasta use (chmod +x *) para dar permissão a todos repositórios ou (chmod +x exemplo) para dar permissão a um único repositório"
+echo "agora vc dará permissão ao termux para ter acesso aos seus arquivos (termux-setup-storage)"
+termux-setup-storage
+echo "execute para instala ferramentas-> (bash bilin)" && echo "Feito por _xXKrill3Xx_" && sleep 10s;
